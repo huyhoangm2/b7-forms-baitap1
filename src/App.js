4ff348d2d91f2b228263a9a97e3fb1bda510fa9a
huyhoangm2 <hoangken760@gmail.com>
2023-03-03 16:42:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-function App() {
+const initialValues = {
+  to: "",
+  subject: "",
+  message: "",
+  attachment: null,
+};
+
+const validationSchema = Yup.object({
+  to: Yup.string().email("Địa chỉ email không hợp lệ").required("Không được để trống"),
+  subject: Yup.string().required("Không được để trống"),
+  message: Yup.string().required("Không được để trống"),
+});
+
+const onSubmit = (values, { setSubmitting }) => {
+  setTimeout(() => {
+    alert(JSON.stringify(values, null, 2));
+    setSubmitting(false);
+  }, 400);
+};
+
+const EmailForm = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Soạn thảo email</h1>
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+        {({ isSubmitting, setFieldValue }) => (
+          <Form>
+            <div>
+              <label htmlFor="to">Người nhận:</label>
+              <Field type="email" name="to" id="to" />
+              <ErrorMessage name="to" component="div" className="error-message" />
+            </div>
+            <div>
+              <label htmlFor="subject">Chủ đề:</label>
+              <Field type="text" name="subject" id="subject" />
+              <ErrorMessage name="subject" component="div" className="error-message" />
+            </div>
+            <div>
+              <label htmlFor="message">Nội dung:</label>
+              <Field as="textarea" name="message" id="message" />
+              <ErrorMessage name="message" component="div" className="error-message" />
+            </div>
+            <div>
+              <label htmlFor="attachment">Upload file:</label>
+              <input type="file" name="attachment" id="attachment" onChange={(event) => setFieldValue("attachment", event.target.files[0])} />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              Gửi
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
 
-export default App;
+export default EmailForm;
